ea4d5b437b8489c0b84a9d227911db1e7098bfc0
Janzen Zarzoso <janzen.zarzoso@gmail.com>
2017-01-12 09:50:00 +0800

    Switch state to props.

--- src/client/components/containers/WidgetListContainer.jsx
+++ src/client/components/containers/WidgetListContainer.jsx
@@ -2,18 +2,7 @@ import React from 'react';
 import WidgetList from '../views/WidgetList.jsx';
 
 export default class WidgetContainerList extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			widgets: [
-				{id: 1, name: 'Sidebar'},
-				{id: 2, name: 'Maps'},
-				{id: 3, name: 'Compass'},
-			]
-		}
-	}
-
 	render() {
-		return <WidgetList {...this.state} />;
+		return <WidgetList {...this.props} />;
 	}
 }
